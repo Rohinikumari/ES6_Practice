8b29e5dfffe56bc0fdb1880db494808be694d31f
Rohini <rohini@frontm.com>
2020-11-14 13:21:30 +0530

    practice es 6

--- block_scoping.js
+++ block_scoping.js
@@ -3,13 +3,13 @@
  * // temporal dead zone
  * const x = 1;
  */
-
- {
-     const fn = function (){return 1}
-     console.log(fn()) // 1
-     {
-         const fn = function(){return 2}
-         console.log(fn()) // 2
-     }
-     console.log(fn()) // 1
- }
+"use strict"
+{
+    function fn() { return 1 }
+    console.log(fn()) // 1
+    {
+        function fn() { return 2 }
+        console.log(fn()) // 2
+    }
+    console.log(fn()) // 1
+}
