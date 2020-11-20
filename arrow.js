eec6b8e7f83c5a66e0a4e3ab264a1699ce1b60a1
Rohini <rohini@frontm.com>
2020-11-21 00:19:58 +0530

    arrow function practice

--- arrow.js
+++ arrow.js
@@ -11,3 +11,15 @@ const arr = [1, 2, 3, 4, 5];
 
 const byTwo = arr.map(num => ({ number: num * 2 }))
 console.log(byTwo)
+
+// lexical this
+var obj = {
+    value: 0,
+    increment : function () {
+        setTimeout(()=>{
+            this.value++;
+            console.log(this.value)
+        },1000)
+    }
+}
+obj.increment()
