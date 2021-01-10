6a9efb1fe638d5f5e070f1c5b9cfde6a966b8973
Rohini <rohini@frontm.com>
2021-01-10 13:39:29 +0530

    ES6 practice

---
+++ Object_properties.js
@@ -0,0 +1,6 @@
+let firstname = 'Rohini'
+let lastname = 'Singh'
+let age = 25
+
+let person = {firstname,lastname,age}
+console.log(person)
--- template_literal.js
+++ template_literal.js
@@ -9,3 +9,8 @@ const message = `My name is Bob and I enjoy ${activities
     })
     .join(', ')}`
 console.log(message)
+
+function tag(strings,...values){
+console.log(strings.raw[0])
+}
+const mes = tag`Hello my name \nis Rohini, and my age is \n25`
