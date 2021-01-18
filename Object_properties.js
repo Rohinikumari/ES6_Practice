a8f1b5abed3cdb9a5a2731d2d8c3f1c7585a97f1
Rohini <rohini@frontm.com>
2021-01-18 21:35:27 +0530

    practice object

--- Object_properties.js
+++ Object_properties.js
@@ -2,16 +2,26 @@ let firstname = 'Rohini'
 let lastname = 'Singh'
 let age = 25
 
-let person = {firstname,lastname,age}
+let person = { firstname, lastname, age }
 console.log(person)
 
 let key = 'lastname'
-function getKey(){
+function getKey() {
     return 'Hello'
 }
 person = {
-    firstname:'Rohini',
-    [key] : 'Singh',
-    [getKey()+'Rohini']:'testing'
+    firstname: 'Rohini',
+    [key]: 'Singh',
+    [getKey() + 'Rohini']: 'testing'
 }
+console.log(person)
+
+person = {
+    firstname: 'Rohini',
+    age: 28,
+    haveABirthday() {
+        this.age++
+    }
+}
+console.log(person.haveABirthday)
 console.log(person)
