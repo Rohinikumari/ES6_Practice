855f58856b59a452778643591a5f28a47af14e19
Rohini <rohini@frontm.com>
2021-01-18 21:25:24 +0530

    object practice

--- Object_properties.js
+++ Object_properties.js
@@ -4,3 +4,14 @@ let age = 25
 
 let person = {firstname,lastname,age}
 console.log(person)
+
+let key = 'lastname'
+function getKey(){
+    return 'Hello'
+}
+person = {
+    firstname:'Rohini',
+    [key] : 'Singh',
+    [getKey()+'Rohini']:'testing'
+}
+console.log(person)
